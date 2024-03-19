4c4ec5052aede7811e819d19957fc77d3a1ef7de
Liam Pendleton <pendle@uw.edu>
2024-03-18 18:37:35 -0700

    lots of changes. This creates two species-specific dataframes that include tag IDs for all observations. Also omits rows where locations were not taken.

--- scripts/data_formatting.R
+++ scripts/data_formatting.R
@@ -1,19 +1,41 @@
 library(here)
 library(dplyr)
 
-# Get the list of files in the directory
-file_list <- list.files(path = here("data", "22_tags_processed_3.10", "CSV"))
+### PIGU data formatting ###
+file_list <- list.files(path = here("data", "PIGU_tags", "Processed_data", "CSV")) #get the list of files in the directory
 
-# Initialize an empty list to store data frames
-dfs <- list()
+dfs <- list() #create empty list to store each data frame
 
-# Loop through each file and read it, skipping the first 5 lines
-for (file in file_list) {
-  df <- read.csv(file.path(here("data", "22_tags_processed_3.10", "CSV"), file), header = FALSE, skip = 5)
-  names(df) <- NULL  #remove column names
-  dfs[[file]] <- df
+ids <- c(44067, 45657, 45658, 44072, 44372, 44505) #list of PIGU tag IDs
+
+# Loop through each file and read it, skipping the first 5 lines of data frame title
+for (i in seq_along(file_list)) {
+  df <- read.csv(file.path(here("data", "PIGU_tags", "Processed_data", "CSV"), file_list[i]), header = FALSE, skip = 5) #read in each CSV
+  df <- mutate(df, ID = ids[i]) #add IDs to each data frame
+  df <- filter(df, df[[9]] != 0.00000)  #remove rows where position is not reported
+  dfs[[i]] <- df #store data frame in list
 }
 
+# Combine all data frames into a single data frame
+PIGU_data <- bind_rows(dfs)
+colnames(PIGU_data) <- c("DAY", "MONTH", "YEAR", "HOUR", "MINUTE", "SECOND", "SoD", "#SAT", "LAT", "LONG", "ALT", "CLOCK_OFFSET", "ACCURACY", "BATTERY", "ID")
 
 
+### RHAU data formatting ###
+file_list <- list.files(path = here("data", "RHAU_tags", "Processed_data", "CSV")) #get the list of files in the directory
 
+dfs <- list() #create empty list to store each data frame
+
+ids <- c(44149, 44167, 44369, 44469, 44720, 45653, 45659, 45663, 45672, 45695, 45701) #list of RHAU tag IDs
+
+# Loop through each file and read it, skipping the first 5 lines of data frame title
+for (i in seq_along(file_list)) {
+  df <- read.csv(file.path(here("data", "RHAU_tags", "Processed_data", "CSV"), file_list[i]), header = FALSE, skip = 5) #read in each CSV
+  df <- mutate(df, ID = ids[i]) #add IDs to each data frame
+  df <- filter(df, df[[9]] != 0.00000)  #remove rows where position is not reported
+  dfs[[i]] <- df #store data frame in list
+}
+
+# Combine all data frames into a single data frame
+RHAU_data <- bind_rows(dfs)
+colnames(RHAU_data) <- c("DAY", "MONTH", "YEAR", "HOUR", "MINUTE", "SECOND", "SoD", "#SAT", "LAT", "LONG", "ALT", "CLOCK_OFFSET", "ACCURACY", "BATTERY", "ID")
