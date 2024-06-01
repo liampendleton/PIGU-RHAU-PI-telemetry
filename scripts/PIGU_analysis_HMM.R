289011941086312e126056c088df3bb156236436
Liam Pendleton <pendle@uw.edu>
2024-05-31 17:29:23 -0700

    lots of attempts to fix known states

--- scripts/PIGU_analysis_HMM.R
+++ scripts/PIGU_analysis_HMM.R
@@ -6,7 +6,7 @@ library(tidyr)
 #########################################################
 ## PIGU DATA
 # Load PIGU data
-PIGU_data <- read.csv(here("data", "PIGU_data", "PIGU_data_UTM.csv"))
+PIGU_data <- read.csv(here("data", "PIGU_data", "PIGU_data_UTM_TEST.csv"))
 PIGU_data$time <- as.POSIXct(PIGU_data$time,tz="UTC") #convert times to POSIX 
 
 PIGU_data <- PIGU_data %>%
@@ -39,7 +39,7 @@ PIGU_data_crw <- PIGU_data_crw %>%
 PIGU_data_crw$tod <- as.numeric(format(PIGU_data_crw$time, "%H")) + as.numeric(format(PIGU_data_crw$time, "%M"))/60 #decimal hours
 
 # Save interpolated data
-# write.table(PIGU_data_crw, file = here("data", "PIGU_data", "PIGU_data_crw.csv"), col.names = TRUE, row.names = FALSE, sep = ",", quote = FALSE)
+# write.table(tracks, file = here("data", "PIGU_data", "PIGU_tracks.csv"), col.names = TRUE, row.names = FALSE, sep = ",", quote = FALSE)
 
 # Finish data
 PIGU_data <- PIGU_data_crw
@@ -68,51 +68,109 @@ tracks$hour <- as.integer(strftime(tracks$time, format = "%H", tz="GMT")) #add c
 tracks <- tracks %>%
   filter(if_all(everything(), ~ !is.na(.)))
 
+# tracks <- tracks %>% filter(ID == "44067")
+
 # Get an idea of dist parameter specifications by plotting step length and turn angle
 # Step length
-hist(tracks$step, breaks = 50, xlim=c(0,4000))
+hist(tracks$step, breaks = 50, xlim=c(0,1500), xlab="Step length (m)")
 summary(tracks$step) #look at mean and median
 sd(tracks$step, na.rm = TRUE)
 # Turn angle
-hist(tracks$angle, breaks = 50)
+hist(tracks$angle, breaks = 25, xlab="Turn angle (radians)")
 summary(tracks$angle)
 sd(tracks$angle, na.rm = TRUE)
 
+
+#################
+# set up states #
 stateNames <- c("resting", "exploring", "foraging")
 
+
+############################
+# initial parameter values #
 stepMu0 <- c(5, 500, 100) #refer to stateNames. low resting movement, high exploring movement, intermediate foraging movement
 stepSig0 <- c(2, 100, 50) #deviations
 stepPar0 <- c(stepMu0, stepSig0)
 
 anglePar0 <- c(9, 6, 0.5) #refer to stateNames. initial concentration parameters; little variation in angle, intermediate, high
 
-# MODEL 1
-# Fit first model without covariates
+
+###################################################
+# use dist2nest to determine forced resting state #
+tracks$rest <- as.double(tracks$dist2nest < 25)
+tracks$rest <- as.factor(tracks$dist2nest < 25)
+tracks$rest <- factor(tracks$dist2nest < 25, levels = c(FALSE, TRUE), labels = c("0", "1")) #convert rest to 1/0
+knownStates <- ifelse(tracks$rest == "1", 1, NA)
+
+
+###########
+# MODEL 1 #
+# Fit first model without covariates, without trying to specify knownStates. If you check object PIGU_HMM_1, it'll give you output of the model
 PIGU_HMM_1 <- fitHMM(data = tracks, nbStates = 3, 
                      dist = list(step="gamma", angle="vm"), 
                      Par0 = list(step=stepPar0, angle=anglePar0),
                      estAngleMean = list(angle=FALSE),
-                     stateNames = stateNames)
+                     stateNames = stateNames,
+                     # knownStates = knownStates
+                     )
 
-# MODEL 2
-# Formula for transition probabilities; add TOD
-formula1 <- ~cosinor(tod, period = 24)
+
+###########
+# MODEL 2 #
+# Formulae from McClintock code
+formula <- ~ 0 + rest
+formulaDelta <- ~ 0 + rest
+
 # Get parameter estimates from previous model to initialize next model
-Par0_2 <- getPar0(model = PIGU_HMM_1, 
-                  formula = formula1)
+Par0_2 <- getPar0(model = PIGU_HMM_1,
+                  formula = formula)
 
-# Use parameter estimates and coefficient from previous model; include TOD covariate
+betaRef <- c(3,2,3) # make state 3 (foraging) the tpm reference for state 1 (resting)
+
+# set up initial regression coefficients; rest0 is "if i'm not resting, how likely am I to do this action?". Rest1 is "if i AM resting..."
+beta0 <- matrix(c(-100, 25, -25, NA, -25, NA,
+                  100, -100, 25, NA, 25, NA),2,3*(3-1),byrow=TRUE,dimnames=list(c("rest0","rest1"),c("1 -> 1", "1 -> 3", "2 -> 1", "2 -> 3", "3 -> 1", "3 -> 2")))
+
+# Use parameter estimates, regression coefficients, TPM, and other info to make a more informed model
 PIGU_HMM_2 <- fitHMM(data = tracks, nbStates = 3, dist = list(step="gamma", angle="vm"),
                      Par0 = list(step=Par0_2$Par$step, angle=Par0_2$Par$angle), 
-                     beta0 = Par0_2$beta,
+                     betaRef = betaRef,
+                     beta0 = beta0,
                      estAngleMean = list(angle=FALSE),
                      stateNames = stateNames,
-                     formula = formula1)
+                     formula = formula,
+                     formulaDelta = formulaDelta,
+                     knownStates = knownStates)
+
+###########
+# MODEL 3 #
+
+# Tweak regression coefficients based on output of previous model
+beta0 <- matrix(c(-75, 25, -25, NA, -15, NA,
+                  75, -75, 25, NA, 15, NA),2,3*(3-1),byrow=TRUE,dimnames=list(c("rest0","rest1"),c("1 -> 1", "1 -> 3", "2 -> 1", "2 -> 3", "3 -> 1", "3 -> 2")))
+
+Par0_3 <- getPar0(model = PIGU_HMM_2,
+                  formula = formula)
+# Things start to break down here. Can't figure out how to hone in on answer.
+PIGU_HMM_3 <- fitHMM(data = tracks, nbStates = 3, dist = list(step="gamma", angle="vm"),
+                     Par0 = list(step=Par0_3$Par$step, angle=Par0_2$Par$angle),
+                     betaRef = betaRef,
+                     beta0 = beta0,
+                     estANgleMean = list(angle=FALSE),
+                     stateNames = stateNames,
+                     formula = formula,
+                     formulaDelta = formulaDelta,
+                     knownStates = knownStates)
+
+############### I suggest just running the full code from McColintock's answer to that guy in the thread and comparing his output to what we get from each model here to see how "rest is handled, and what its output looks like
 
 
 # MODEL VALIDATION
-states <- viterbi(PIGU_HMM_1) #decode most likely state sequence; change out HMM models
-table(states)/nrow(tracks) #derive percentage of time spent in each state
+states <- viterbi(PIGU_HMM_2) #decode most likely state sequence; change out HMM models
+# table(states)/nrow(tracks) #derive percentage of time spent in each state
+plot(PIGU_HMM_2,plotCI=TRUE,ask=FALSE)
+unique(viterbi(PIGU_HMM_2)[which(tracks$rest==0)])
+unique(viterbi(PIGU_HMM_2)[which(tracks$rest==1)])
 
 # Compare models
 AIC(PIGU_HMM_1, PIGU_HMM_2)
